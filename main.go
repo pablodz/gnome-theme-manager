2e7930a1a9eff1bfc359b333a11f57a3420010f3
Pablo Diaz <pablo.diazv@pucp.edu.pe>
2022-12-30 14:59:02 -0400

    first release

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+
+	a := app.New()
+	w := a.NewWindow("Gnome Theme Manager")
+
+	hello := widget.NewLabel("Gnome Theme Manager!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
+
+	w.ShowAndRun()
+}
